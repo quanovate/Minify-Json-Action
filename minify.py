0a215c3792f6c3029fb25d2e16b9d31833a44515
Matt Dunbar <matt@mattdsworld.com>
2025-01-13 10:54:49 +0700

    Update to support multiline comment removal

--- minify.py
+++ minify.py
@@ -18,8 +18,7 @@ def main(args):
     content = None
     print('Reading file {:}'.format(in_path))
     with open(in_path, 'r', encoding='utf-8') as f_in:
-        content = f_in.read()
-        content = re.sub("/\\*.*?\\*/", "", content)
+        content = remove_comments(f_in.read())
         print("Parsing {:}".format(in_path))
         print(content)
         content = json.loads(content)
@@ -30,3 +29,17 @@ def main(args):
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
+def remove_comments(string):
+    pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
+    # first group captures quoted strings (double or single)
+    # second group captures comments (//single-line or /* multi-line */)
+    regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
+    def _replacer(match):
+        # if the 2nd group (capturing comments) is not None,
+        # it means we have captured a non-quoted (real) comment string.
+        if match.group(2) is not None:
+            return "" # so we will return empty to remove the comment
+        else: # otherwise, we will return the 1st group
+            return match.group(1) # captured quoted-string
+    return regex.sub(_replacer, string)
